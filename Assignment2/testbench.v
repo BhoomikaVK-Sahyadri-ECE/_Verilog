3eea8a8ff1e200dfea56ba09c565d98fffb6fc28
BhoomikaVK-Sahyadri-ECE <bhoomikavk.ec22@sahyadri.edu.in>
2025-06-01 15:32:13 +0530

    Create testbench.v

---
+++ Assignment2/testbench.v
@@ -0,0 +1,16 @@
+module tb_adder();
+  reg a,b,c;
+  wire sum,carry;
+  adder uut(a,b,c,sum,carry);
+  initial begin
+  $dumpfile("dump.vcd");
+    $dumpvars(1, tb_adder); 
+  end
+  initial begin
+      a=1'b0;b=1'b1;c=1'b0; #10
+      a=1'b0;b=1'b1;c=1'b1; #10
+      a=1'b1;b=1'b0;c=1'b0; #10
+      a=1'b1;b=1'b1;c=1'b1; #10
+      $finish;
+    end
+      endmodule
